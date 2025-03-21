8e416f26af7f9fff2cc874422d48c108a6f73773
yohaboy <yohannesgizaw777@gmail.com>
2025-03-21 20:24:23 +0300

    query

--- base/views.py
+++ base/views.py
@@ -3,14 +3,22 @@ from django.http import HttpResponse
 from accounts.models import User
 from .models import Post
 from django.views.generic import ListView
+from django.db.models import Q
 
 def home(request):
 
+    results = []
     posts = Post.objects.all()
     user_exists = User.objects.filter(username=request.user.username)
-
+    
+    if 'search' in request.GET:
+        query = request.GET['search']
+        results = Post.objects.filter(Q(user__username__icontains=query)| Q(category__icontains=query))
+    if results:
+        posts = results
     context = {
         'user_exists':user_exists,
         'posts':posts
     }
     return render(request,'base/index.html', context=context)
+
