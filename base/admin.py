151d859a66e9f89188e020f0160270ffb8c04219
yohaboy <yohannesgizaw777@gmail.com>
2025-03-21 11:06:18 +0300

    custom-adminPanel

--- base/admin.py
+++ base/admin.py
@@ -1,7 +1,28 @@
-from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-from accounts.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
+class CustomUserAdmin(UserAdmin):
 
-admin.site.register(User, UserAdmin)
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'profile'),
+        }),
+    )
+
+    # Fields to display in the admin user edit form
+    fieldsets = (
+        (None, {'fields': ('username', 'email', 'password')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+        ('Important dates', {'fields': ('last_login', 'date_joined')}),
+        ('Profile', {'fields': ('profile',)}),  # Add the profile field here
+    )
+
+    # Fields to display in the user list view
+    list_display = ('username', 'email', 'is_staff', 'profile')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
+
+admin.site.register(User, CustomUserAdmin)
 
